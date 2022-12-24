0ebab56b59edd3aea0a30ad0a9e61b7aadae613e
Baladurage Kavinda <dilshankavinda371@gmail.com>
2022-12-24 20:08:58 +0530

    Add login page

--- app/src/main/java/com/example/sourthenlankacarrental/MainActivity.java
+++ app/src/main/java/com/example/sourthenlankacarrental/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.sourthenlankacarrental;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -41,8 +44,14 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(MainActivity.this,Dashboard.class);
-                startActivity(intent);
+                Intent intent=new Intent(MainActivity.this,Login.class);
+
+                Pair[] pairs=new Pair[2];
+                pairs[0]=new Pair<View,String>(image,"logo_image");
+                pairs[1]=new Pair<View,String>(name,"logo_text");
+
+                ActivityOptions options=ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                startActivity(intent,options.toBundle());
                 finish();
             }
         },SPLASH_SCREEN);
