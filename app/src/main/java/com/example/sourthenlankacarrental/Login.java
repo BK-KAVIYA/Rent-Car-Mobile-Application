d0ca336d3778c3038eeb50cde9a741d27d8b53f9
Baladurage Kavinda <dilshankavinda371@gmail.com>
2022-12-26 20:47:01 +0530

    sing up page

--- app/src/main/java/com/example/sourthenlankacarrental/Login.java
+++ app/src/main/java/com/example/sourthenlankacarrental/Login.java
@@ -2,15 +2,58 @@ package com.example.sourthenlankacarrental;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Login extends AppCompatActivity {
 
+    Button callSingup,login_btn;
+    ImageView image;
+    TextView logoText,sloganText;
+    TextInputLayout username,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        //Hooks
+        callSingup= findViewById(R.id.singup_screen);
+        image= findViewById(R.id.LogiImage);
+        logoText= findViewById(R.id.logo_name);
+        sloganText= findViewById(R.id.slogo_name);
+        username= findViewById(R.id.username);
+        password= findViewById(R.id.password);
+        login_btn= findViewById(R.id.login_btn);
+
+        callSingup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Login.this,SingUp.class);
+
+                Pair[] pairs=new Pair[7];
+
+                pairs[0]=new Pair<View,String>(image,"logo_image");
+                pairs[1]=new Pair<View,String>(logoText,"logo_text");
+                pairs[2]=new Pair<View,String>(sloganText,"logo_desc");
+                pairs[3]=new Pair<View,String>(username,"user_tran");
+                pairs[4]=new Pair<View,String>(password,"password_tran");
+                pairs[5]=new Pair<View,String>(login_btn,"login_tran");
+                pairs[6]=new Pair<View,String>(callSingup,"button_tran");
+
+                ActivityOptions options=ActivityOptions.makeSceneTransitionAnimation(Login.this,pairs);
+                startActivity(intent,options.toBundle());
+
+            }
+        });
     }
 }
