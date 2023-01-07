5777216ca6fc748456546690fbf0663f46432d95
Baladurage Kavinda <dilshankavinda371@gmail.com>
2023-01-07 11:08:34 +0530

    update

--- app/src/main/java/com/example/sourthenlankacarrental/SingUp.java
+++ app/src/main/java/com/example/sourthenlankacarrental/SingUp.java
@@ -3,14 +3,102 @@ package com.example.sourthenlankacarrental;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SingUp extends AppCompatActivity {
+
+    //variable
+    TextInputLayout regName,userName,email,phonenNumber,idNumber,Password,cPassword;
+    Button regBtn,regToLoginbtn;
+    ProgressBar progressBar;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sing_up);
+
+
+        //Hooks to all xml element in activity sing up
+        regName=findViewById(R.id.name);
+        userName=findViewById(R.id.uname);
+        email=findViewById(R.id.email);
+        phonenNumber=findViewById(R.id.phonenumber);
+        idNumber=findViewById(R.id.idnumber);
+        Password=findViewById(R.id.password);
+        cPassword=findViewById(R.id.confirmpassword);
+        regBtn=findViewById(R.id.reg_btn);
+        regToLoginbtn=findViewById(R.id.reg_login_btn);
+        progressBar=findViewById(R.id.progress_bar);
+
+        //save data in firebase on button click
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                //get all the values
+                String name=regName.getEditText().getText().toString();
+                String uName=userName.getEditText().getText().toString();
+                String Email=email.getEditText().getText().toString();
+                String phoneNumber=phonenNumber.getEditText().getText().toString();
+                String IDNumber=idNumber.getEditText().getText().toString();
+                String Pwd=Password.getEditText().getText().toString();
+                String cPwd=cPassword.getEditText().getText().toString();
+
+                boolean isValidated=validateData(Email,Pwd,cPwd);
+                if (!isValidated){
+                    return;
+                }else {
+                    rootNode=FirebaseDatabase.getInstance();
+                    reference=rootNode.getReference("users");
+
+                    UserHelperClass helperClass=new UserHelperClass(name,uName,Email,phoneNumber,IDNumber,Pwd,cPwd);
+                    reference.child(IDNumber).setValue(helperClass);
+                }
+
+
+            }
+        });
+
+
+    }
+
+    void changeInProgress(boolean inProgress){
+        if (inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            
+        }
+    }
+    boolean validateData(String testEmail,String TPassword,String TcPassword){
+        //validate user enter data
+        if(!Patterns.EMAIL_ADDRESS.matcher(testEmail).matches()){
+            email.setError("Email is Invalid");
+            return false;
+        }
+        if (TPassword.length()<6){
+            email.setError("");
+            Password.setError("Password Length Invaild");
+            return false;
+        }
+        if (!TPassword.equals(TcPassword)){
+            Password.setError("");
+            cPassword.setError("Password not matched");
+            return false;
+        }
+        return true;
+
     }
 }
---
+++ app/src/main/java/com/example/sourthenlankacarrental/UserHelperClass.java
@@ -0,0 +1,75 @@
+package com.example.sourthenlankacarrental;
+
+public class UserHelperClass {
+    //variable
+    String regName,userName,email,phonenNumber,idNumber,Password,cPassword;
+
+    public UserHelperClass() {
+    }
+
+    public UserHelperClass(String regName, String userName, String email, String phonenNumber, String idNumber, String password, String cPassword) {
+        this.regName = regName;
+        this.userName = userName;
+        this.email = email;
+        this.phonenNumber = phonenNumber;
+        this.idNumber = idNumber;
+        Password = password;
+        this.cPassword = cPassword;
+    }
+
+    public String getRegName() {
+        return regName;
+    }
+
+    public void setRegName(String regName) {
+        this.regName = regName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhonenNumber() {
+        return phonenNumber;
+    }
+
+    public void setPhonenNumber(String phonenNumber) {
+        this.phonenNumber = phonenNumber;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getcPassword() {
+        return cPassword;
+    }
+
+    public void setcPassword(String cPassword) {
+        this.cPassword = cPassword;
+    }
+}
